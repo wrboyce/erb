78de37c097248e99acf557c72459ad5f4059cded
Will Boyce <me@willboyce.com>
2011-06-12 17:01:16 +0100

    added `erb_handler_seen` — bringing basic .seen functionality (volatile, ets backed)

---
+++ src/event_handlers/erb_handler_seen.erl
@@ -0,0 +1,145 @@
+%% -------------------------------------------------------------------
+%% @author Will Boyce <me@willboyce.com> [http://willboyce.com]
+%% @copyright 2011 Will Boyce
+%% @doc erb_channel
+%% -------------------------------------------------------------------
+-module(erb_handler_seen).
+-author("Will Boyce").
+-behaviour(gen_event).
+
+%% Include files
+-include("erb.hrl").
+
+%% API
+-export([start_link/0, add_handler/0]).
+
+%% gen_event callbacks
+-export([init/1, handle_event/2, handle_call/2, handle_info/2, terminate/2, code_change/3]).
+
+%% Server macro
+-define(SERVER, ?MODULE).
+
+%% State record
+-record(state, {db}).
+
+%% ===================================================================
+%% gen_event callbacks
+%% ===================================================================
+%% -------------------------------------------------------------------
+%% @spec start_link() -> {ok,Pid} | {error,Error}
+%% @doc Creates an event manager.
+%% -------------------------------------------------------------------
+start_link() ->
+    gen_event:start_link({local, ?SERVER}).
+
+%% -------------------------------------------------------------------
+%% @spec add_handler() -> ok | {'EXIT',Reason} | term()
+%% @doc Adds an event handler
+%% -------------------------------------------------------------------
+add_handler() ->
+    gen_event:add_handler(?SERVER, ?MODULE, []).
+
+%% ===================================================================
+%% gen_event callbacks
+%% ===================================================================
+%% -------------------------------------------------------------------
+%% @spec init(Args) -> {ok, State}
+%% @doc Whenever a new event handler is added to an event manager,
+%% this function is called to initialize the event handler.
+%% -------------------------------------------------------------------
+init([]) ->
+    {ok, #state{db=ets:new(seen_ets, [set, private])}}.
+
+%% -------------------------------------------------------------------
+%% @spec
+%% handle_event(Event, State) -> {ok, State} |
+%%                               {swap_handler, Args1, State1, Mod2, Args2} |
+%%                               remove_handler
+%% @docWhenever an event manager receives an event sent using
+%% gen_event:notify/2 or gen_event:sync_notify/2, this function is called for
+%% each installed event handler to handle the event.
+%% -------------------------------------------------------------------
+handle_event(Data = #data{}, State) ->
+    case string:str(Data#data.origin, "!") > 0 of
+        true ->
+            [Nick|_] = string:tokens(Data#data.origin, "!"),
+            Key = {Nick, Data#data.destination},
+            ets:insert(State#state.db, {Key, Data}),
+            case Data#data.operation of
+                privmsg ->
+                    case string:tokens(Data#data.body, " ") of
+                        [".seen", SeenNick] ->
+                            SeenKey = {SeenNick, Data#data.destination},
+                            Response = io_lib:format("~s: ", [Nick]) ++
+                                case ets:lookup(State#state.db, SeenKey) of
+                                    [{SeenKey, SeenData}] ->
+                                        {SeenAgeDays, {SeenAgeHours,SeenAgeMinutes,SeenAgeSeconds}} = calendar:time_difference(SeenData#data.datetime, calendar:universal_time()),
+                                        io_lib:format("~s was last seen", [SeenNick]) ++
+                                        string:join("", lists:map(fun({V,L}) ->
+                                            case V of
+                                                0 ->
+                                                    "";
+                                                _ ->
+                                                    io_lib:format(" ~p ~s", [V, L])
+                                            end
+                                        end, [{SeenAgeDays, "days"}, {SeenAgeHours, "hours"}, {SeenAgeMinutes, "minutes"}])) ++ io_lib:format(" ~p seconds ago", [SeenAgeSeconds]);
+                                    [] ->
+                                        io_lib:format("I haven't seen ~s", [SeenNick])
+                                end,
+                            ok = gen_server:cast({global, erb_dispatcher}, {privmsg, Data#data.destination, Response});
+                        _ ->
+                            ok
+                    end;
+                _ ->
+                    ok
+            end;
+        false ->
+            ok
+    end,
+    {ok, State}.
+
+%% -------------------------------------------------------------------
+%% @spec
+%% handle_call(Request, State) -> {ok, Reply, State} |
+%%                                {swap_handler, Reply, Args1, State1,
+%%                                  Mod2, Args2} |
+%%                                {remove_handler, Reply}
+%% @doc Whenever an event manager receives a request sent using
+%% gen_event:call/3,4, this function is called for the specified event
+%% handler to handle the request.
+%% -------------------------------------------------------------------
+handle_call(_Request, State) ->
+    Reply = ok,
+    {ok, Reply, State}.
+
+%% -------------------------------------------------------------------
+%% @spec
+%% handle_info(Info, State) -> {ok, State} |
+%%                             {swap_handler, Args1, State1, Mod2, Args2} |
+%%                              remove_handler
+%% @doc This function is called for each installed event handler when
+%% an event manager receives any other message than an event or a synchronous
+%% request (or a system message).
+%% -------------------------------------------------------------------
+handle_info(_Info, State) ->
+    {ok, State}.
+
+%% -------------------------------------------------------------------
+%% @spec terminate(Reason, State) -> void()
+%% @docWhenever an event handler is deleted from an event manager,
+%% this function is called. It should be the opposite of Module:init/1 and
+%% do any necessary cleaning up.
+%% -------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%% -------------------------------------------------------------------
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @doc Convert process state when code is changed
+%% -------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%% ===================================================================
+%% Internal functions
+%% ===================================================================
