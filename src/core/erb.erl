07d70b83bf219a895026572da8f707de2161b600
Will Boyce <me@willboyce.com>
2011-08-22 23:38:05 +0100

    Added `erb.erl` - convenience module for configuration

--- src/core/erb.erl
+++ src/core/erb.erl
@@ -1,8 +1,34 @@
 -module(erb).
--export([add_server/2, add_bot/3]).
+-export([add_server/2, add_bot/3, add_module/2, add_admin/2]).
 
 add_server(Network, {Host, Port}) ->
     ok = gen_server:cast({global, erb_config_server}, {addServer, Network, {Host, Port}}).
 
 add_bot(Nick, Network, Chans) ->
     ok = gen_server:cast({global, erb_bot_manager}, {addBot, Nick, Network, Chans}).
+
+add_module(BotId, Module) ->
+    Modules = case gen_server:call({global, erb_config_server}, {getConfig, BotId, modules}) of
+        {ok, Config} -> Config;
+        noconfig -> []
+    end,
+    case lists:member(Module, Modules) of
+        false ->
+            NewModules = [Module|Modules],
+            ok = gen_server:cast({global, erb_config_server}, {putConfig, BotId, modules, NewModules});
+        _ ->
+            pass
+    end.
+
+add_admin(BotId, Admin) ->
+    Admins = case gen_server:call({global, erb_config_server}, {getConfig, BotId, admins}) of
+        {ok, Config} -> Config;
+        noconfig -> []
+    end,
+    case lists:member(Admin, Admins) of
+        false ->
+            NewAdmins = [Admin|Admins],
+            ok = gen_server:cast({global, erb_config_server}, {putConfig, BotId, admins, NewAdmins});
+        _ ->
+            pass
+    end.
