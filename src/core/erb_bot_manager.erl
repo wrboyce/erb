3388e25f306462562dd2b5a7c11fca2b3a9f2163
Will Boyce <me@willboyce.com>
2011-08-22 20:37:30 +0100

    un-exporting debug exports

--- src/core/erb_bot_manager.erl
+++ src/core/erb_bot_manager.erl
@@ -8,7 +8,7 @@
 -include("erb.hrl").
 
 %% API
--export([start_link/0]).
+-export([start_link/0, start_bot/1]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
